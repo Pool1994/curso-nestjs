dd3e83ba5b4decd79f4febf658ab2aa6edcbb34c
Pool <pool.vera@alarcongroup.us>
2024-04-16 18:15:05 -0500

    fix

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,24 +1,27 @@
+require('dotenv').config();
 module.exports = {
   "development": {
-    "username": "root",
-    "password": "password",
-    "database": "nestjs_course",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "port":3307
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_CONNECTION,
+    "port": process.env.DB_PORT
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_CONNECTION,
+    "port": process.env.DB_PORT
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_CONNECTION,
+    "port": process.env.DB_PORT
   }
 }
